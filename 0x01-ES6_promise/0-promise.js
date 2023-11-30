15e7a1a3e2bce2cdf66a0d5c0b987400c856fc0b
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-11-30 04:24:40 +0300

    handle reject and error

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,9 +2,15 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     const successMessage = 'successful message or data retrieved';
+    const errorSimulation = true;
+
     setTimeout(() => {
-      console.log(`Yay! some message ${successMessage}`);
-      resolve('some data fromthe API');
+      if (errorSimulation) {
+        reject(new Error('Error: Unable to retrieve data from the API'));
+      } else {
+        console.log(`Yay! some message ${successMessage}`);
+        resolve('some data fromthe API');
+      }
     }, 2000);
   });
 }
