c591548859870c9398985153f1256a8f38562950
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-12-04 01:24:07 +0300

    initialize function to return array

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+// implement a function to return an array
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
+
+export default initializeRooms;
