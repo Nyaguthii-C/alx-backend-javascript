18dea4607d537b8a5366f0e074063f05e6a9f74c
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-02-19 12:21:12 +0300

    use process for stdni

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,15 @@
-const readLine= require('node:readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+/*
+ * Using Process stdin
+*/
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
-readLine.question(`Welcome to Holberton School, what is your name?`, name => {
-  console.log(`Your name is:${name}!`);
-  readLine.close();
-  console.log('This important software is now closing')
+
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });
