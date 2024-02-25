6b7910da6d2c118334e82e03771bda0bd8009226
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-02-25 04:42:57 +0300

    Create a new function to equate to calculator in sum mode

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,12 @@
+/*
+ * calls the Utils.calculateNumber function with type SUM
+ */
+const Utils = require('./utils');
+
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
