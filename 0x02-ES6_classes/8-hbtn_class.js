c2e150f573efc968bc0c7fa426d193afb3cf211e
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-03-11 15:37:06 +0300

    implement eslint

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,7 +1,7 @@
 /**
 When the class is cast into a Number, it should return the size.
 When the class is cast into a String, it should return the location.
-**/
+* */
 export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
@@ -11,5 +11,6 @@ export default class HolbertonClass {
   [Symbol.toPrimitive](hint) {
     if (hint === 'string') return this._location;
     if (hint === 'number') return this._size;
+    return 1; // for eslint
   }
 }
