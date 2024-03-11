199689947dd7b8b3af1dff6239731e89cd1c304c
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-03-11 15:16:55 +0300

    inherit class car in class evcar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,16 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  //when cloneCar is called on a EVCar object, 
+  //the object returned should be an instance of Car instead of EVCar
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
+
+export default EVCar;
