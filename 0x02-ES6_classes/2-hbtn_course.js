918ebe43664a40dcc05daad579f01e449a98d3a9
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-03-11 14:46:26 +0300

    create course class, add setters and getters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,26 +1,41 @@
-export default class Currency {
-  constructor(code, name) {
-    this._code = code;
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('name must be a string');
+    if (typeof length !== 'number') throw TypeError('length must be a number');
+    if (students.constructor !== Array && students.every((el) => typeof el === 'string')) {
+      throw TypeError('students must be an array of strings');
+    }
     this._name = name;
-  }
-
-  get code() {
-    return this._code;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set code(newCode) {
-    this._code = newCode;
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
   }
 
   set name(newName) {
+    if (typeof newName !== 'string') throw TypeError('name must be a string');
     this._name = newName;
   }
 
-  displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+  set length(newLength) {
+    if (typeof newLength !== 'number') throw TypeError('length must be a number');
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    if (newStudents.constructor !== Array && newStudents.every((el) => typeof el === 'string')) {
+      throw TypeError('students must be an array of strings');
+    }
+    this._students = newStudents;
   }
 }
